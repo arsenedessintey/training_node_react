0c8ab862861d7ce111ce3753112234b760326c1b
arsenedessintey <arsene.buzenet@dessintey.com>
2023-05-23 18:56:21 +0200

    Add comment

--- client/src/component/serverHello.tsx
+++ client/src/component/serverHello.tsx
@@ -1,24 +1,30 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const ServerHello = () => {
+    // State to store the message received from the server
     const [message, setMessage] = useState<string | null>(null);
 
+    // Function to handle the button click event
     const click = () => {
+        // Fetch data from the "/api" endpoint
         fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setMessage(data.message));
+            .then((res) => res.json()) // Parse the response as JSON
+            .then((data) => setMessage(data.message)); // Update the message state with the received message
     }
 
-    return (<div>
-        {/* if message not nul display it */}
-        {message ?? <p>message</p>}
-        <hr />
+    return (
+        <div>
+            {/* Display the content of the message variable if it is not null */}
+            {/* use '{}' to include javascript inside html */}
+            {message && <p>{message}</p>}
+            <hr />
 
-        <button 
-            type="button"
-            onClick={click}
-        >
-            Click to say hello
-        </button>
-    </div>)
+            <button
+                type="button"
+                onClick={click} // Call the click function when the button is clicked
+            >
+                Click to say hello
+            </button>
+        </div>
+    );
 }
--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,35 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'
+import cors from 'cors';
 
+// Load environment variables from a .env file
 dotenv.config();
 
+// Create an instance of the Express application
 const app: Express = express();
-app.use(cors())
+
+// Apply the CORS middleware to enable Cross-Origin Resource Sharing
+app.use(cors());
+
+// Set the server port by reading the value from the PORT environment variable
 const port = process.env.PORT;
 
+// Handler for the root route '/'
 app.get('/', (req: Request, res: Response) => {
+  // Respond with the string 'Express + TypeScript Server'
   res.send('Express + TypeScript Server');
 });
 
+// Handler for the '/api' route
 app.get('/api', (req: Request, res: Response) => {
-  console.log(`request from ${req.url}`)
-  res.status(200).json({message: "Hello from server"});
+  // Log the request URL
+  console.log(`request from ${req.url}`);
+  // Respond with a JSON object containing the message 'Hello from server'
+  res.status(200).json({ message: "Hello from server" });
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
+  // Print a message indicating that the server is running and specify the URL
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
