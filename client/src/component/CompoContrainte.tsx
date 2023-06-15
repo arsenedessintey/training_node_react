607705ec36e80c82964a5610a1c253d384109744
adriendessintey <adrien.chomat@dessintey.com>
2023-06-15 13:42:26 +0200

    Groupe fin

--- client/src/component/CompoContrainte.tsx
+++ client/src/component/CompoContrainte.tsx
@@ -6,9 +6,11 @@ import image3 from "./Modif.webp"
 import image4 from "./fleche-gauche.webp"
 import image5 from "./+.webp"
 import image6 from "./Fgauche2.webp"
-import Groupe from "./Groupe";
 
-
+interface Groupeinter {
+  idg: any
+  nomGroupe: string
+}
 
 const CompoContrainte = () => {
   // Modal Contrainte
@@ -27,6 +29,18 @@ const CompoContrainte = () => {
     getConstraint()
 
   }, []);
+
+  //GROUPE STATE
+
+  const timeId = new Date().getTime()
+
+  //
+
+  const [groupes, setGroupe] = useState<Groupeinter[]>([]);
+
+  const [nouveauGroupe, setNouveauGroupe] = useState("");
+
+  //FIN GROUPE STATE
 
   async function getConstraint() {
     const tmpConstraint: Constraint[] = (await axios.get('/api/tabCon')).data;
@@ -47,8 +61,8 @@ const CompoContrainte = () => {
 
 //Modal Groupe
 
-  const toggleModal2 = (constraint: Constraint | undefined) => {
-    setSelectConstraint(constraint);
+  const toggleModal2 = () => {
+
     setModal2(!modal2);
   };
 
@@ -74,6 +88,40 @@ const CompoContrainte = () => {
 
   }
 
+  //GROUPE//
+
+    const handleDeleteGroupe = (idg: any) => {
+      const groupeCopy = [...groupes];
+      const groupeUpt = groupeCopy.filter((groupee) => groupee.idg !== idg);
+      setGroupe(groupeUpt);
+  }
+
+
+  const handleChangeGroupe = (e: any) => {
+
+      const valueAfter = e.target.value;
+      setNouveauGroupe(valueAfter);
+
+  };
+
+  const handleSubmitGroupe = (e: any) => {
+
+      e.preventDefault();
+      const groupeCopy = [...groupes];
+      const idg = timeId
+      const nomGroupe = nouveauGroupe;
+      groupeCopy.push({ idg, nomGroupe });
+      setGroupe(groupeCopy);
+      setNouveauGroupe("");
+      toggleModal2()
+
+  }
+
+  {constraint}
+
+  //FIN GROUPE//
+
+
   return (
     <>
       {modal && (
@@ -82,7 +130,15 @@ const CompoContrainte = () => {
       )}
 
       {modal2 && (
-        <Groupe togm2={() => toggleModal2(undefined)}/>
+
+        <div className="divcadregroupe">
+            <div className="cadregroupe">
+                <input type="text" className='inputgroupe' placeholder="Nom du Groupe..."  value={nouveauGroupe} onChange={handleChangeGroupe} />
+                <div className="divbgroupe">
+                    <button type="submit" className="buttongroupe" onClick={handleSubmitGroupe}>+</button>
+                </div>
+            </div>
+        </div>
 
       )}
 
@@ -90,9 +146,22 @@ const CompoContrainte = () => {
         <input type="image" className="Fgauche2" src={image6} />
       </div>
       <div className="divfiche">
+
+        {groupes.map((groupe) => (
+          <li key={groupe.idg} className="textgroupe">
+
+          <input type="image" className="croix2groupe" src={image2} onClick={() => handleDeleteGroupe(groupe.idg)}/><label>-- </label> {groupe.nomGroupe} <label>-- </label>
+
+          </li>
+
+        ))}
+        <li>
+
+        </li>
+
         <div className="divgroupe">
           <div className="groupe">
-            <input type="image" className="plus" src={image5} onClick={() => {toggleModal2(undefined)}} />
+            <input type="image" className="plus" src={image5} onClick={() => {toggleModal2()}} />
             <p>INSERER UN GROUPE</p>
           </div>
         </div>
@@ -101,7 +170,7 @@ const CompoContrainte = () => {
         <div className="bacCong">
           <ul>
             {constraint.map(constr =>
-              <div className="nnconstraint">
+              <div key={constr.nom} className="nnconstraint">
                 <div className="nconstraint">
                   <input type="image" className="croix2" src={image2} onClick={() => { if (window.confirm("Attention tu vas supprimé une contrainte")) { handledelete(constr.contrainte_id) } }} />
                   <input type="image" className="Modif" src={image3} onClick={() => { toggleModal(constr) }} />
